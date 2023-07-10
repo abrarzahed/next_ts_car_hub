f34307c1f05c48acfd774ec3fd00316b6fdf9b16
abrarzahed <abrarzahed6986@gmail.com>
2023-07-10 23:21:21 +0600

    model search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,47 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import SearchManufacturer from "./SearchManufacturer";
+import SearchButton from "./SearchButton";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export const SearchBar = () => {
+  const router = useRouter();
   const [manufacturer, setManufacturer] = useState("");
-  const handleSearch = () => {};
+  const [model, setModel] = useState("");
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === "" && model === "") {
+      return alert("Please fll in the search bar");
+    }
+
+    updateSearchParams(
+      model.toLocaleLowerCase(),
+      manufacturer.toLocaleLowerCase()
+    );
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathName = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathName);
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
@@ -14,7 +50,27 @@ export const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          alt="model icon"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+        />
+        <input
+          type="text"
+          name="name"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiquan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 };
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export const fetchCars = async () => {
   const headers = {
     "X-RapidAPI-Key": "39a1b415f9mshf07f62daae79c1bp18ad6ajsnfb0a6cd63236",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
     {
       headers: headers,
     }
@@ -27,4 +29,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery" || "");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
